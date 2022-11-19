7f43162c97156a8c037dd11824ca749c3dd6ea40
Riikka Holopainen <riikka.holopainen@movedoc.fi>
2022-11-19 12:43:34 +0200

    exercise 3 and some other stuff

--- Data/create_alc.R
+++ Data/create_alc.R
@@ -1,10 +1,13 @@
 # Riikka Holopainen 12.11.2022 Data wrangling exercise, data source: UCI Machine Learning Repository, Student Performance Data (incl. Alcohol consumption) page https://archive.ics.uci.edu/ml/datasets/Student+Performance
 
 library(tidyverse)
-student_mat <- read.csv("data/student-mat.csv")
-student_por <- read.csv("data/student-por.csv")
+student_mat <- read.csv("data/student-mat.csv", sep = ";", header = TRUE)
+student_por <- read.csv("data/student-por.csv", sep = ";", header = TRUE)
 
 dim(student_mat)
-dim(student_por)
 str(student_mat)
+summary(student_mat)
+
+dim(student_por)
 str(student_por)
+summary(student_mat)
